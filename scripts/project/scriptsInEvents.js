aeaa7dbc001e961a2107e9b6e748272cb2fea84c
nhungrose25 <160495719+nhungrose25@users.noreply.github.com>
2025-02-11 19:50:04 +0700

    Add files via upload

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -3,7 +3,7 @@
 
 const scriptsInEvents = {
 
-	async Main_Event1_Act4(runtime, localVars)
+	async Main_Event1_Act5(runtime, localVars)
 	{
 		
 		var backgroundDiv = document.createElement("div");
@@ -24,6 +24,44 @@ const scriptsInEvents = {
 		
 		
 		document.body.appendChild(backgroundDiv);
+	},
+
+	async Main_Event14_Act1(runtime, localVars)
+	{
+		function randomPickByPercentage(options) {
+		    const random = Math.random() * 100;
+		    let cumulativePercentage = 0;
+		
+		    for (const option of options) {
+		        cumulativePercentage += option.percentage;
+		        if (random <= cumulativePercentage) {
+		            return option.value;
+		        }
+		    }
+		    return null; 
+		}
+		
+		
+		const arrayInstance = runtime.objects["award"].getFirstInstance();
+		const options = [
+		    { value: 0, percentage: arrayInstance.getAt(0,5) },
+		    { value: 1, percentage: arrayInstance.getAt(1,5) },
+		    { value: 2, percentage: arrayInstance.getAt(2,5) },
+		    { value: 3, percentage: arrayInstance.getAt(3,5) },
+			{ value: 4, percentage: arrayInstance.getAt(4,5) },
+		    { value: 5, percentage: arrayInstance.getAt(5,5) },
+			{ value: 6, percentage: arrayInstance.getAt(6,5) },
+			{ value: 7, percentage: arrayInstance.getAt(7,5) },
+			{ value: 8, percentage: arrayInstance.getAt(8,5) },
+			{ value: 9, percentage: arrayInstance.getAt(9,5) },
+			{ value: 10, percentage: arrayInstance.getAt(10,5) },
+			{ value: 11, percentage: arrayInstance.getAt(11,5) }
+		];
+		
+		
+		const result = randomPickByPercentage(options);
+		
+		runtime.globalVars.get = result;
 	}
 
 };
